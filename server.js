0fe2808d9fea2046d0c9688b88a6d6ca9d57f0e6
Jeny Alejandra Mazo <yenymzo@gmail.com>
2017-12-20 13:00:37 -0500

    updating readme

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 const Speaker = require('./api/models/speakerModel');
 const Talk = require('./api/models/talkModel');
 const bodyParser = require('body-parser');
-const Schema = mongoose.Schema;
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-//mongodb://<dbuser>:<dbpassword>@ds161016.mlab.com:61016/talks
 mongoose.connect('mongodb://medjs:medjs2017@ds161016.mlab.com:61016/talks');
-// const userSchema = new Schema({
-//     name  :  { type: String, default: 'medjs' },
-//     password   :  { type: String, default: 'medjs2017' }
-// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// app.use(function(req, res) {
+//     if (res.statusCode == 404){
+//         return res.status(404).send({url: req.originalUrl + 'not found'});
+//     }
+// });
 
 const routes = require('./api/routes/routes.js'); //importing route
 routes(app); //register the route
@@ -25,3 +24,4 @@ routes(app); //register the route
 app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
+
